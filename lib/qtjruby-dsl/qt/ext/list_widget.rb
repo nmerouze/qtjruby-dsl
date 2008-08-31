955bdbb79704689dd5d0e240de98da7bf443421a
Nicolas Mérouze <nicolas.merouze@gmail.com>
2008-08-31 18:51:52 +0200

    Add << to Qt::ListWidget.

--- lib/qtjruby-dsl/qt/ext/list_widget.rb
+++ lib/qtjruby-dsl/qt/ext/list_widget.rb
@@ -5,7 +5,12 @@ module Qt
       def self.included(base)
         base.class_eval do
           def items=(items)
+            clear
             add_items(items)
+          end
+          
+          def <<(item)
+            add_item(item)
           end
         end
       end
